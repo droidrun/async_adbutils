ac12692ceb6e23c3b06945f0e0ce188457fbd380
johnmalek312 <77510343+johnmalek312@users.noreply.github.com>
2025-11-10 21:28:35 +1100

    clean setup

--- setup.py
+++ setup.py
@@ -3,15 +3,7 @@
 #
 # Licensed under MIT
 #
-# https://docs.travis-ci.com/user/deployment/pypi/
 
 import setuptools
-import subprocess
-import sys
 
-print("setup.py arguments:", sys.argv)
-
-if sys.argv[-1] == "build_wheel":
-    subprocess.call([sys.executable, "build_wheel.py"])
-else:
-    setuptools.setup(setup_requires=['pbr'], python_requires='>=3.10', pbr=True)
+setuptools.setup()
